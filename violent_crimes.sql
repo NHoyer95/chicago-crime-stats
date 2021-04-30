6a70f7340ca3479786d53204fa38711f18dbb564
NHoyer95 <hoyer047@umn.edu>
2021-04-29 21:45:04 -0500

    Completed data load to SQL db

---
+++ violent_crimes.sql
@@ -0,0 +1,16 @@
+drop table violent_crimes_db;
+
+create table violent_crimes (
+	id serial primary key,
+	Date date, 
+	Primary_Type varchar,
+	Description varchar, 
+	Arrest boolean,
+	Domestic boolean,
+	District int,
+	Year int,
+	Latitude float,
+	Longitude float
+);
+
+select * from violent_crimes;
