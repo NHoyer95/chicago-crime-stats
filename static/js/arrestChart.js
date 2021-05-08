55d405873edf68c18759b43b8d16b94770d2b2b2
NHoyer95 <hoyer047@umn.edu>
2021-05-08 13:53:55 -0500

    Updated html routes

--- static/js/arrestChart.js
+++ static/js/arrestChart.js
@@ -1,25 +1,23 @@
 console.log("arrestChart.js loaded");
 
 d3.json("/arrestChartData", function (data) {
-
-    // ... and dump that JSON to the console for inspection
     console.log(data); 
 
       
-    //     var chartData = [{
-    //           type: "sunburst",
-    //           maxdepth: 2,
-    //           ids: ,
-    //           labels: unpack(rows, 'labels'),
-    //           parents: unpack(rows, 'parents'),
-    //           textposition: 'inside',
-    //           insidetextorientation: 'radial'
-    //     }]
+    var chartData = [{
+        type: "sunburst",
+        maxdepth: 2,
+        ids: ,
+        labels: ,
+        parents: ,
+        textposition: 'inside',
+        insidetextorientation: 'radial'
+        }]
       
-    //     var layout = {margin: {l: 0, r: 0, b: 0, t:0}}
+        var layout = {margin: {l: 0, r: 0, b: 0, t:0}}
       
-    //     Plotly.newPlot('myDiv', data, layout)
-    //   })
+        Plotly.newPlot('myDiv', data, layout)
+      })
 });
 
 // function InitDashboard() {
