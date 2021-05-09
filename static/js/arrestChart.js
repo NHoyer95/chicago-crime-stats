c21c85cbcab98c4372cacc35829471706e1260da
NHoyer95 <hoyer047@umn.edu>
2021-05-09 15:42:32 -0500

    Updated sunburst - completed

--- static/js/arrestChart.js
+++ static/js/arrestChart.js
@@ -3,7 +3,7 @@ console.log("arrestChart.js loaded");
 d3.json("/arrestChartData", function(data){
 console.log(data); 
 
-
+// Function to enable groupBy function
 function groupBy(list, keyGetter) {
   const map = new Map();
   list.forEach((item) => {
@@ -24,47 +24,64 @@ var primaryTypes = groupBy(data, crime => crime.primary_type);
 // Homicide stats
 var homicide = primaryTypes.get("HOMICIDE");
 var numberHomicide = homicide.length;
-
-var homicideArrest = groupBy(homicide, arrest => arrest.arrest).get(true);
-var numHomicideArrest = homicideArrest.length;
-var homicideNoArrest = groupBy(homicide, arrest => arrest.arrest).get(false);
-var numHomicideNoArrest = homicideNoArrest.length;
+var numHomicideArrest = groupBy(homicide, arrest => arrest.arrest).get(true).length;
+var numHomicideNoArrest = groupBy(homicide, arrest => arrest.arrest).get(false).length;
 
 // Battery stats
 var battery = primaryTypes.get("BATTERY")
 var numberBattery = battery.length;
+var numBatteryArrest = groupBy(battery, arrest => arrest.arrest).get(true).length;
+var numBatteryNoArrest = groupBy(battery, arrest => arrest.arrest).get(false).length;
 
-var batteryArrest = groupBy(battery, arrest => arrest.arrest).get(true);
-var numBatteryArrest = batteryArrest.length;
-var batteryNoArrest = groupBy(battery, arrest => arrest.arrest).get(false);
-var numBatteryNoArrest = batteryNoArrest.length;
+// Assault stats
+var assault = primaryTypes.get("ASSAULT")
+var numberAssault = assault.length;
+var numAssaultArrest = groupBy(assault, arrest => arrest.arrest).get(true).length;
+var numAssaultNoArrest = groupBy(assault, arrest => arrest.arrest).get(false).length;
 
-// var battery = primaryTypes.get("BATTERY")
-// var numberBattery = battery.length;
+// Robbery stats
+var robbery = primaryTypes.get("ROBBERY")
+var numberRobbery = robbery.length;
+var numRobberyArrest = groupBy(robbery, arrest => arrest.arrest).get(true).length;
+var numRobberyNoArrest = groupBy(robbery, arrest => arrest.arrest).get(false).length;
 
-// var batteryArrest = groupBy(battery, arrest => arrest.arrest).get(true);
-// var numBatteryArrest = batteryArrest.length;
-// var batteryNoArrest = groupBy(battery, arrest => arrest.arrest).get(false);
-// var numBatteryNoArrest = batteryNoArrest.length;
+// Sex Offense stats
+var so = primaryTypes.get("SEX OFFENSE")
+var numberSO = so.length;
+var numSOArrest = groupBy(so, arrest => arrest.arrest).get(true).length;
+var numSONoArrest = groupBy(so, arrest => arrest.arrest).get(false).length;
 
-console.log(numberBattery);
+console.log(primaryTypes);
 
 var arrestSunburst = [{
   type: "sunburst",
-  labels: ["Homicide", "Homicide Arrest = True", "Homicide Arrest = False", 
-  "Battery", "Battery Arrest = True", "Battery Arrest = False"],
-  parents: ["", "Homicide", "Homicide", "", "Battery", "Battery"],
-  values:  [numberHomicide, numHomicideArrest, numHomicideNoArrest, numberBattery, numBatteryArrest, numBatteryNoArrest],
-  outsidetextfont: {size: 20, color: "#377eb8"},
+  ids: ["Homicide", "Homicide Arrest = True", "Homicide Arrest = False", 
+  "Battery", "Battery Arrest = True", "Battery Arrest = False",
+  "Assault", "Assault Arrest = True", "Assault Arrest = False",
+  "Robbery", "Robbery Arrest = True", "Robbery Arrest = False",
+  "Sex Offense", "Sex Offense Arrest = True", "Sex Offense Arrest = False"],
+  labels: ["Homicide", "Arrested", "Not Arrested", 
+  "Battery", "Arrested", "Not Arrested",
+  "Assault", "Arrested", "Not Arrested",
+  "Robbery", "Arrested", "Not Arrested",
+  "Sex Offense", "Arrested", "Not Arrested"],
+  parents: ["", "Homicide", "Homicide", "", "Battery", "Battery", "", "Assault", "Assault", 
+  "", "Robbery", "Robbery", "", "Sex Offense", "Sex Offense"],
+  values:  [numberHomicide, numHomicideArrest, numHomicideNoArrest, numberBattery, numBatteryArrest, 
+  numBatteryNoArrest, numberAssault, numAssaultArrest, numAssaultNoArrest, numberRobbery, numRobberyArrest, 
+  numRobberyNoArrest, numberSO, numSOArrest, numSONoArrest],
+  textfont: {size: 20},
   leaf: {opacity: 0.4},
-  marker: {line: {width: 2}},
-  branchvalues: 'total'
+  marker: {line: {width: 1}, colorscale: "Blues"},
+  branchvalues: "total",
+  textposition: "inside",
+  insidetextorientation: "radial"
 }];
 
 var layout = {
-  margin: {l: 0, r: 0, b: 0, t: 0},
-  width: 500,
-  height: 500
+  margin: {l: 0, r: 0, b: 40, t: 0},
+  width: 900,
+  height: 900
 };
 
 
